7ce3eb20290f82325aab8017ffa634a6423d6950
Simon Hellström <simonhellstromsson97@gmail.com>
2025-12-09 14:01:58 +0100

    Roulette wheel

--- src/components/Roulette.vue
+++ src/components/Roulette.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="segment" :style="segmentStyle">
+    <div class="segment" :style="numbersStyle">
         <div class="segment-inner" :style="innerStyle">
             {{ number }}
         </div>
@@ -22,16 +22,13 @@ export default {
         angle(){
             return this.index * this.angleStep;
         },
-        segmentStyle(){
-            return {
+        
 
-                
-               transform:
-          "rotate(" +
-          this.angle +
-          "deg) translateY(-" +
-          this.radius +
-          "px)"
+        numbersStyle(){
+            return {
+            
+                transform: `translate(-50%, -50%) rotate(${this.angle}deg) translateY(-${this.radius}px)`
+          
             };
         },
         innerStyle(){
@@ -41,12 +38,12 @@ export default {
             };
         },
         color(){
-            if (this.number===0) return "rgb(0,128,0)";
+            if (this.number===0) return "rgb(0,150,0)";
             const red = new Set([1, 3, 5, 7, 9, 12, 14, 16, 18,
         19, 21, 23, 25, 27, 30, 32, 34, 36
             ]);
             return red.has(this.number)
-            ? "rgb(220,53,69)"
+            ? "rgb(200,0,20)"
             : "rgb(10,10,10)";
         }
     }
@@ -57,18 +54,20 @@ export default {
 <style scoped>
 .segment{
     position:absolute;
-    top:39%;
-    left:46%;
-    width: 27px;
+    top:50%;
+    left:50%;
+    width: 31px;
     height: 40px;
-    transform-origin: 50% 100%;
+    transform-origin: 50% 50%;
+    z-index: 2;
 }
 
 .segment-inner{
     width: 100%;
     height: 100%;
     border-radius: 5px;
-    border: 2px solid black;
+    border: 3px solid rgb(192, 192, 192);
+    
     color: white;
     font-weight: bold;
     font-size: 14px;
