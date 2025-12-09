5119cb77f3578b39942837306c151f59b33bf47c
Matteus <matteus.sandstrom@gmail.com>
2025-12-09 14:29:21 +0100

    todays work

--- src/components/RatingTool.vue
+++ src/components/RatingTool.vue
@@ -2,8 +2,8 @@
     <div class="rating">
         <header>Rate 1-10</header>
         <div class="rating-buttons">
-            <button class="rating-button" v-for="score in scores" @click="rating=score"
-                :class="{'selected': score === rating }">{{ score }}</button>
+            <button class="rating-button" v-for="score in scores" @click="setRating(score)"
+                :class="{ 'selected': score === rating }">{{ score }}</button>
         </div>
     </div>
 </template>
@@ -14,6 +14,11 @@ export default {
         return {
             scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             rating: null
+        }
+    },
+    methods: {
+        setRating(number) {
+            this.rating = number;
         }
     }
 }
@@ -26,8 +31,8 @@ export default {
     flex-wrap: wrap;
     flex-direction: column;
     background-color: var(--Caribbean_Green);
-    padding:1rem;
-    gap:1rem;
+    padding: 1rem;
+    gap: 1rem;
 
 }
 
@@ -37,25 +42,27 @@ export default {
     color: var(--Metallic_Yellow);
     border-bottom: 10px solid var(--Metallic_Yellow);
 }
-.rating-buttons{
+
+.rating-buttons {
     display: grid;
     grid-template-rows: repeat(2, 1fr);
     grid-template-columns: repeat(5, 1fr);
     gap: 1rem;
 }
-.rating-button{
-font-family: "Science Gothic", sans-serif;
-height: 10rem;
-width: auto;
-font-size: 3rem;
-background-color: beige;
-}
-.rating-button.selected{
-font-family: "Science Gothic", sans-serif;
-height: 10rem;
-width: auto;
-font-size: 3rem;
-background-color: green;
+
+.rating-button {
+    font-family: "Science Gothic", sans-serif;
+    height: 10rem;
+    width: auto;
+    font-size: 3rem;
+    background-color: beige;
 }
 
+.rating-button.selected {
+    font-family: "Science Gothic", sans-serif;
+    height: 10rem;
+    width: auto;
+    font-size: 3rem;
+    background-color: green;
+}
 </style>
