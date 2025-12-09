be481efe4bfc534919fe9d3a390748a9a777663f
Matteus <matteus.sandstrom@gmail.com>
2025-12-09 12:54:59 +0100

    commit for push

--- src/views/minigames/drawing/HostDrawingView.vue
+++ src/views/minigames/drawing/HostDrawingView.vue
@@ -1,14 +1,21 @@
 <template>
-    <h1>All drawings</h1>
-    <div v-for="painting in submittedPaintings">
-        <img :src="painting.data"></img>
-        <h1>{{context.state.players.find(x => x.id == painting.playerId).name}}</h1>
-    </div>
+    <div class="drawings">
 
-    <p>{{ context.state.players }}</p>
+        <h1>All drawings</h1>
+        <div>What do you like?</div>
+        <div v-for="painting in submittedPaintings">
+            <img :src="painting.data"></img>
+            <h1>{{context.state.players.find(x => x.id == painting.playerId).name}}</h1>
+        </div>
+        <div class="showcase"></div>
+        <div class="tools">Rate on you pgone</div>
+        <p>{{ context.state.players }}</p>
+    </div>
 </template>
 
 <script>
+//access via https://localhost:5173/game?id=draw&mode=host
+
 import { context } from "../../../context";
 import { socket } from "../../../socket";
 
@@ -34,4 +41,19 @@ export default {
 
 </script>
 
-<style></style>
+<style>
+.drawings {
+    background-color: black;
+    color: red;
+    display: flex;
+    flex-direction: column;
+
+    height: 100%;
+    width: 100%;
+}
+
+.showcase {
+    background: yellow;
+    flex-grow: 1;
+}
+</style>
