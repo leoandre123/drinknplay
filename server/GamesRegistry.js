c20f3d6fbc0c30631be7d0399be5740578d175ac
Simon Hellström <simonhellstromsson97@gmail.com>
2025-12-30 15:55:09 +0100

    baxade GamesRegistry

---
+++ server/GamesRegistry.js
@@ -0,0 +1,7 @@
+import { ClosestWin } from "./minigames/ClosestWin.js";
+import { DrawingGame } from "./minigames/DrawingGame.js";
+import { KahootGame } from "./minigames/KahootGame.js";
+import { RacingGame } from "./minigames/RacingGame.js";
+import { ReactionGame } from "./minigames/ReactionGame.js";
+
+export const ALL_GAMES = [RacingGame, KahootGame, DrawingGame, ReactionGame, ClosestWin];
