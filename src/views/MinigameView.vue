1bc5fe9ec2ed7b9862e547c446f344e403b43c82
Matteus <matteus.sandstrom@gmail.com>
2026-01-09 13:26:00 +0100

    Fixed

--- src/views/MinigameView.vue
+++ src/views/MinigameView.vue
@@ -1,49 +1,22 @@
 <template>
-  <div v-if="!isFullscreen">
-    <div v-if="context.isHost" class="container">
-      <div class="title-bar">
-        <h1 v-if="context.state.gameIndex == 0">2.5D Racer</h1>
-        <h1 v-if="context.state.gameIndex == 1">Questions game</h1>
-      </div>
-      <div class="body-container">
-        <div class="scoreboard-container">
-          <p v-for="player in context.state.players">{{ player.name }}</p>
-        </div>
-        <div class="minigame-container">
-          <RacingView v-if="context.state.gameIndex == 0" class="minigame" />
-          <KahootView v-if="context.state.gameIndex == 1" class="minigame" />
-          <HostDrawingView v-if="context.state.gameIndex == 2" class="minigame" />
-          <ReactionGame v-if="context.state.gameIndex == 3" class="minigame" />
-        </div>
-      </div>
+  <div class="fullscreen">
+    <div v-if="context.isHost" class="minigame-container">
+      <RacingView v-if="context.state.gameIndex == 0" class="minigame" />
+      <KahootView v-if="context.state.gameIndex == 1" class="minigame" />
+      <HostDrawingView v-if="context.state.gameIndex == 2" class="minigame" />
+      <ReactionGame v-if="context.state.gameIndex == 3" class="minigame" />
+      <ClosestView v-if="context.state.gameIndex == 4" class="minigame" />
+      <HostMazeGameView v-if="context.state.gameIndex == 5" class="minigame" />
+      <RouletteView v-if="context.state.gameIndex == 6" class="minigame" />
     </div>
     <div v-if="!context.isHost" class="client-container">
       <RacingControllerView v-if="context.state.gameIndex == 0" class="minigame" />
       <KahootPlayerView v-if="context.state.gameIndex == 1" class="minigame" />
       <PlayerDrawingView v-if="context.state.gameIndex == 2" class="minigame" />
       <ReactionGamePlayerView v-if="context.state.gameIndex == 3" class="minigame" />
-    </div>
-  </div>
-  <div v-if="isFullscreen">
-    <div class="fullscreen">
-      <div v-if="context.isHost" class="minigame-container">
-        <RacingView v-if="context.state.gameIndex == 0" class="minigame" />
-        <KahootView v-if="context.state.gameIndex == 1" class="minigame" />
-        <HostDrawingView v-if="context.state.gameIndex == 2" class="minigame" />
-        <ReactionGame v-if="context.state.gameIndex == 3" class="minigame" />
-        <ClosestView v-if="context.state.gameIndex == 4" class="minigame" />
-        <HostMazeGameView v-if="context.state.gameIndex == 5" class="minigame" />
-        <RouletteView v-if="context.state.gameIndex == 6" class="minigame" />
-      </div>
-      <div v-if="!context.isHost" class="client-container">
-        <RacingControllerView v-if="context.state.gameIndex == 0" class="minigame" />
-        <KahootPlayerView v-if="context.state.gameIndex == 1" class="minigame" />
-        <PlayerDrawingView v-if="context.state.gameIndex == 2" class="minigame" />
-        <ReactionGamePlayerView v-if="context.state.gameIndex == 3" class="minigame" />
-        <ClosestPlayerView v-if="context.state.gameIndex == 4" class="minigame" />
-        <PlayerMazeGameView v-if="context.state.gameIndex == 5" class="minigame" />
-        <RoulettePlayerView v-if="context.state.gameIndex == 6" class="minigame" />
-      </div>
+      <ClosestPlayerView v-if="context.state.gameIndex == 4" class="minigame" />
+      <PlayerMazeGameView v-if="context.state.gameIndex == 5" class="minigame" />
+      <RoulettePlayerView v-if="context.state.gameIndex == 6" class="minigame" />
     </div>
   </div>
 </template>
--- src/views/minigames/drawing/PlayerDrawingView.vue
+++ src/views/minigames/drawing/PlayerDrawingView.vue
@@ -1,108 +1,147 @@
 <template>
-    <div class="player-view-container">
-        <button v-if="gamePhase==='start'"
-        class="start-button"
-        @click = "startGame">START GAME</button>
+  <div class="player-view-container">
+    <button v-if="gamePhase === 'start'" class="start-button" @click="startGame">START GAME</button>
 
-        <div class="drawing-canvas" v-if="gamePhase === 'drawing'">
-            <div class="drawing-title">Drink n' Draw</div>
-            <DrawingCanvas :options="drawingOptions" ref="canvas">
-            </DrawingCanvas>
-            <DrawingTools :options="drawingOptions" @save-requested="saveCanvas">
-            </DrawingTools>
-        </div>
-
-        <div class="rating" v-if="gamePhase == 'voting'">
-            <RatingTool 
-            v-if="canVote"
-            :key="currentDrawingToVote.socketId"
-            @raiting-submitted="playerRated">
-            </RatingTool>
-        </div>
+    <div v-if="gamePhase === 'drawing'" class="drawing-canvas" :style="{ flexDirection: isMobile ? 'row' : 'column' }">
+      <div v-if="!isMobile" class="drawing-title">Drink n' Draw</div>
+      <DrawingColors v-if="isMobile" :options="drawingOptions" direction="column" />
+      <div class="canvas-container">
+        <DrawingCanvas :options="drawingOptions" ref="canvas" />
+      </div>
+      <div v-if="!isMobile" class="tools-container">
+        <DrawingColors :options="drawingOptions" direction="row" />
+        <DrawingTools :options="drawingOptions" @save-requested="saveCanvas" direction="row" />
+      </div>
+      <DrawingTools
+        v-if="isMobile"
+        :options="drawingOptions"
+        @save-requested="saveCanvas"
+        direction="column"
+      />
     </div>
+
+    <div class="rating" v-if="gamePhase == 'voting'">
+      <RatingTool
+        v-if="canVote"
+        :key="currentDrawingToVote.socketId"
+        @raiting-submitted="playerRated"
+      >
+      </RatingTool>
+    </div>
+  </div>
 </template>
 
 <script>
-import DrawingCanvas from '../../../components/DrawingCanvas.vue';
-import DrawingTools from '../../../components/DrawingTools.vue';
-import RatingTool from '../../../components/RatingTool.vue';
-
+import DrawingColors from "@/components/DrawingColors.vue";
+import RatingTool from "../../../components/RatingTool.vue";
+import DrawingCanvas from "@/components/drawingCanvas.vue";
+import DrawingTools from "@/components/drawingTools.vue";
 
 import { context } from "../../../context";
 import { socket } from "../../../socket";
-
+import { useDevice } from "@/UseDevice";
 
 export default {
-    components: { DrawingCanvas, DrawingTools, RatingTool },
-    data() {
-        return {
-            drawingOptions: {
-                brushColor: "black",
-                brushSize: 10,
-                isBucketSelected: false,
-            },
-            gamePhase: "",
-            canvasPNG: null,
-            currentDrawingToVote: null,
-            
-        };
+  components: { DrawingCanvas, DrawingTools, RatingTool, DrawingColors },
+  data() {
+    return {
+      drawingOptions: {
+        brushColor: "black",
+        brushSize: 10,
+        isBucketSelected: false,
+      },
+      gamePhase: "",
+      canvasPNG: null,
+      currentDrawingToVote: null,
+    };
+  },
+  setup() {
+    const { isMobile } = useDevice();
+    return { isMobile };
+  },
+  computed: {
+    canVote() {
+      if (!this.currentDrawingToVote) return false;
+      return this.currentDrawingToVote.socketId !== socket.id;
     },
-    computed: {
-        canVote(){
-            if (!this.currentDrawingToVote) return false;
-            return this.currentDrawingToVote.socketId !== socket.id;
-        }
-
+  },
+  mounted() {
+    socket.on("gamePhase", (phaseFromServer) => {
+      this.gamePhase = phaseFromServer;
+    });
+    socket.on("drawingToVote", (drawingFromServer) => {
+      this.currentDrawingToVote = drawingFromServer;
+    });
+  },
+  methods: {
+    startGame() {
+      socket.emit("startDrawingGame");
+      console.log("Start request sent!");
     },
-    mounted() {
-        socket.on("gamePhase", (phaseFromServer) => { this.gamePhase = phaseFromServer });
-        socket.on("drawingToVote", (drawingFromServer) => {this.currentDrawingToVote = drawingFromServer
-        });
+    saveCanvas() {
+      this.canvasPNG = this.$refs.canvas.getCanvas();
+      socket.emit("updateCanvas", this.canvasPNG);
     },
-    methods: {
-        startGame(){
-        socket.emit("startDrawingGame")
-        console.log("Start request sent!")
+    playerRated(score) {
+      console.log("SCORES ADDED: " + score);
+      socket.emit(
+        "playerVote",
+        (score = {
+          score: score,
+          socketId: this.currentDrawingToVote.socketId,
+        })
+      );
     },
-        saveCanvas() {
-            this.canvasPNG = this.$refs.canvas.getCanvas();
-            socket.emit("updateCanvas", this.canvasPNG);
-        },
-        playerRated(score) {
-            console.log("SCORES ADDED: " +score)
-            socket.emit("playerVote", score = {
-                score: score,
-                socketId: this.currentDrawingToVote.socketId
-            })
-        }
-
-    }
+  },
 };
 </script>
 
 <style scoped>
 .drawing-canvas {
-    position: relative;
-    background: linear-gradient(90deg, rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(252, 176, 69, 1) 87%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-repeat: repeat;
+  position: relative;
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 50%,
+    rgba(252, 176, 69, 1) 87%
+  );
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-repeat: repeat;
+  height: 100%;
+  width: 100%;
+}
+
+.canvas-container {
+  flex-grow: 1;
+  aspect-ratio: 16/9;
+  min-height: 0;
+  min-width: 0;
+  max-width: 90%;
+  max-height: 90%;
 }
 
 .drawing-title {
-    font-size: 3rem;
-    font-family: "Science Gothic", sans-serif;
-    color: var(--Metallic_Yellow);
-    text-shadow: 3px 3px black;
-    border-bottom: 1px solid var(--Metallic_Yellow);
-    width: 70%;
-    margin-bottom: 1rem;
+  font-size: 3rem;
+  font-family: "Science Gothic", sans-serif;
+  color: var(--Metallic_Yellow);
+  text-shadow: 3px 3px black;
+  border-bottom: 1px solid var(--Metallic_Yellow);
+  width: 70%;
+  margin-bottom: 1rem;
 }
 
-.start-button{
-font-size: 3vw;
-height: 20vw;
-width: 30vw;;
+.tools-container {
+  display: flex;
+  background-color: gray;
+  height: 4rem;
+  border: 5px outset black;
+}
+
+.start-button {
+  font-size: 3vw;
+  height: 20vw;
+  width: 30vw;
 }
 </style>
